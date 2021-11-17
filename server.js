9415d0d7fd83eb38c442b953669223f31de505d8
Jillium <86424914+Jillium@users.noreply.github.com>
2021-11-17 14:49:52 -0700

    insomnia test post request

--- server.js
+++ server.js
@@ -5,14 +5,21 @@ const { notes } = require('./Develop/db/db.json');
 // const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 
+
 const PORT = process.env.PORT || 3003;
 const app = express();
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// gets notes from the db.json file 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+})
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
 })
 
 // app.use(express.static('public'));
