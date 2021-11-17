00cdd061d49748c94dd76cbdefca0a087581e0db
Jillium <86424914+Jillium@users.noreply.github.com>
2021-11-17 13:21:42 -0700

    initial commit, server running and note link fixed

---
+++ server.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3003;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
