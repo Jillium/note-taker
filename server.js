fb195e5d4525c34635d3c54231dd32eb7de22da9
Jillium <86424914+Jillium@users.noreply.github.com>
2021-11-17 14:17:15 -0700

    route to notes on db.json

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 const express = require('express');
+const { notes } = require('./Develop/db/db.json');
 // const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3003;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-app.use(express.static('public'));
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
+
+// app.use(express.static('public'));
 // app.use('/api', apiRoutes);
 // app.use('/', htmlRoutes);
 
